07d7f105f8ec9799ec223becc5f0139cd7f0ae95
MohammedTayibulla <contact2tayib@gmail.com>
2024-12-26 18:35:36 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
 
 export default function Home() {
+  console.log("Home")
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
